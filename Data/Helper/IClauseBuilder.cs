a4de5073442442b9c9ea658193a4a778bae80b68
antonio <antony.trino@gmail.com>
2021-01-02 19:24:37 -0300

    Refatoração da lógica de pesquisa

---
+++ Data/Helper/IClauseBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace Data.Helper
+{
+    public interface IClauseBuilder
+    {
+        public string GetClause(PropertyInfo property);
+        public string GetStringLikeClause();
+        public string GetStringEqualClause();
+        public string GetEqualClause(string value = null);
+        public string GetDateTimeClause(Type pType);
+        public string GetNumberClause<T>() where T : IComparable<T>;
+        public string GetEnumClause(PropertyInfo property);
+    }
+}
--- Data/Repositories/RepositoryExtension.cs
+++ Data/Repositories/RepositoryExtension.cs
@@ -49,86 +49,17 @@ namespace Data.Repositories
 
             foreach (var (propName, value) in searchParams)
             {
-                var clause = GetClause(propName, value.ToString());
+                var property = typeof(T).GetProperty(propName);
+                if (property == null)
+                    throw new KeyNotFoundException(
+                        $"Attribute '{propName}' not found in entity '{typeof(T).Name}'");
+                
+                var clause = new ClauseBuilder(typeof(T).Name, propName, value).GetClause(property);
 
                 if (!string.IsNullOrEmpty(clause))
                     results = results.WhereDynamic(x => clause);
             }
             return results;
-        }
-
-        private string GetClause(string propName, string value)
-        {
-            var property = typeof(T).GetProperty(propName);
-            if(property == null)
-                throw new KeyNotFoundException(
-                    $"Attribute '{propName}' not found in entity '{typeof(T).Name}'");
-            var pType = property.PropertyType;
-            string clause = null;
-
-            if (pType == typeof(string))
-            {
-                clause = $"x.{propName}.Contains(\"{value}\")";
-            }
-            else if (pType == typeof(DateTime))
-            {
-                if (DateTime.TryParse(value, out var dateTime))
-                    clause = $"x.{propName}.Date.Equals((DateTime)(object)\"{dateTime}\")";
-                else
-                    throw new InvalidCastException(
-                        $"Invalid date for attribute '{propName}' in entity '{typeof(T).Name}'");
-            }
-            else if (pType == typeof(bool) || pType == typeof(long))
-            {
-                clause = $"x.{propName} == {value}";
-            }
-            else if (property.PropertyType.IsEnum)
-            {
-                var enumNames = property.PropertyType.GetEnumNames().ToList();
-                var idx = enumNames.IndexOf(value);
-                if(idx == -1)
-                    throw new InvalidEnumArgumentException(
-                        $"Invalid value for attribute '{propName}' in entity '{typeof(T).Name}'");
-                clause = $"x.{propName} == {idx + 1}";
-            }
-            return clause;
-        }
-    }
-
-    public class Metadata
-    {
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-        public int PagesSize { get; set; }
-        public int TotalCount { get; set; }
-
-        public bool HasPrevious => CurrentPage > 1;
-        public bool HasNext => CurrentPage < TotalPages;
-    }
-
-    public class PagedResult<T> : List<T>
-    {
-        public Metadata Metadata { get; set; }
-
-        public PagedResult(IQueryable<T> items, int count, int pageNumber, int pageSize)
-        {
-            Metadata = new Metadata
-            {
-                TotalCount = count,
-                PagesSize = pageSize,
-                CurrentPage = pageNumber,
-                TotalPages = (int) Math.Ceiling(count / (double) pageSize)
-            };
-
-            AddRange(items);
-        }
-
-        public static PagedResult<T> ToPagedResult(IEnumerable<T> source, int pageNumber, int pageSize)
-        {
-            var count = source.Count();
-            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-            
-            return new PagedResult<T>(items, count, pageNumber, pageSize);
         }
     }
 }
